281a10024bf6d1df633c4c93b4fde28be0a874de
Juho Autio <juho.autio@rovio.com>
2014-04-03 10:24:43 +0300

    Added mkdirs for conf file generation. Generating the file directly in the right place, no rename required any more. When reading, validating that the placeholder values have been replaced with something else.

--- src/main/java/juhoautio/whentodrive/configuration/Configuration.java
+++ src/main/java/juhoautio/whentodrive/configuration/Configuration.java
@@ -3,6 +3,7 @@ package juhoautio.whentodrive.configuration;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import juhoautio.whentodrive.json.JsonUtils;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,32 +23,12 @@ public class Configuration {
     public static final String DT_USER_AGENT = "DT-User-Agent";
     public static final String DT_CONTACT_INFO = "DT-Contact-Info";
 
-    private static final Logger LOG = LoggerFactory.getLogger(Configuration.class);
+    public static final String PLACEHOLDER_USER_AGENT = "TODO: appname-version";
+    public static final String PLACEHOLDER_CONTACT_INFO = "TODO: name@example.com";
 
-    public static Configuration read() {
-        InputStream conf = Configuration.class.getResourceAsStream("/conf.json");
-        if (conf == null) {
-            File templateFile = new File("src/main/resources/conf-template.json");
-            String msg = "No conf.json found. Writing a template to " + templateFile.getAbsolutePath() +
-                    "\nAdd the missing values and rename to conf.json";
-            LOG.error(msg);
-            HashMap<String, String> userMap = new HashMap<>();
-            userMap.put(DT_USER_AGENT, null);
-            userMap.put(DT_CONTACT_INFO, null);
-            Configuration template = new Configuration(userMap);
-            try {
-                JsonUtils.PRETTY_PRINTER.writeValue(templateFile, template);
-            } catch (IOException e) {
-                throw new RuntimeException("Writing conf.json template failed", e);
-            }
-            throw new IllegalArgumentException(msg);
-        }
-        try {
-            return JsonUtils.PRETTY_PRINTER.readValue(conf, Configuration.class);
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to read configuration", e);
-        }
-    }
+    public static final String CONF_PATH = "src/main/resources/conf.json";
+
+    private static final Logger LOG = LoggerFactory.getLogger(Configuration.class);
 
     private final Map<String, String> clientUser;
 
@@ -58,6 +39,63 @@ public class Configuration {
 
     public Map<String, String> getClientUser() {
         return clientUser;
+    }
+
+    /**
+     * Reads configuration from conf.json in classpath.
+     *
+     * @return configuration
+     */
+    public static Configuration read() {
+        InputStream conf = Configuration.class.getResourceAsStream("/conf.json");
+        if (conf == null) {
+            return generateConfigurationFile();
+        }
+        Configuration read;
+        try {
+            read = JsonUtils.PRETTY_PRINTER.readValue(conf, Configuration.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read configuration", e);
+        }
+        validate(read);
+        return read;
+    }
+
+    private static void validate(Configuration read) {
+        checkPlaceHolderReplaced(read, DT_USER_AGENT, PLACEHOLDER_USER_AGENT);
+        checkPlaceHolderReplaced(read, DT_CONTACT_INFO, PLACEHOLDER_CONTACT_INFO);
+    }
+
+    private static void checkPlaceHolderReplaced(Configuration read, String key, String placeholderValue) {
+        String value = read.getClientUser().get(key);
+        if (placeholderValue.equals(value)) {
+            throw new IllegalArgumentException("Placeholder value (" + placeholderValue + ") still left for '" + key
+                    + "' - fix this in " + CONF_PATH);
+        }
+    }
+
+    // write conf.json with all required configuration
+    private static Configuration generateConfigurationFile() {
+        File templateFile = new File(CONF_PATH);
+        if (templateFile.exists()) {
+            throw new RuntimeException("conf.json exists, but it cannot be found in the classpath - fix your " +
+                    "classpath. Building the project again might help.");
+        }
+        String msg = "No conf.json found." +
+                "\nWriting a template to " + templateFile.getAbsolutePath() +
+                "\nAdd the missing values!\n";
+        LOG.error(msg);
+        HashMap<String, String> userMap = new HashMap<>();
+        userMap.put(DT_USER_AGENT, PLACEHOLDER_USER_AGENT);
+        userMap.put(DT_CONTACT_INFO, PLACEHOLDER_CONTACT_INFO);
+        Configuration template = new Configuration(userMap);
+        try {
+            FileUtils.forceMkdir(templateFile.getParentFile());
+            JsonUtils.PRETTY_PRINTER.writeValue(templateFile, template);
+        } catch (IOException e) {
+            throw new RuntimeException("Writing conf.json template failed", e);
+        }
+        throw new IllegalArgumentException(msg);
     }
 
 }
